f53442ebc17d716960b25a58e5d949237af48b04
emorgan05 <emorgan05@gmail.com>
2018-05-01 11:41:52 -0400

    Creating store in index.js and setting up react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={HomeComponent} />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
