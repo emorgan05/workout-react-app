c8efc1a1c36d666b9b1d3dad2d22d5ca32ca16db
emorgan05 <emorgan05@gmail.com>
2018-05-15 16:05:42 -0400

    Working on categoryReducer

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,10 +1,10 @@
 export function categoryReducer(state = {}, action) {
   switch(action.type) {
     case 'LOADING_CATEGORIES':
-      return Object.assign({}, {loading: true});
+      return Object.assign({}, state, {loading: true});
 
     case 'FETCH_CATEGORIES':
-      return Object.assign({}, {loading: false, categories: [].concat(action.payload) });
+      return Object.assign({}, state, {loading: false, categories: [].concat(action.payload) });
 
     default:
       return state;
