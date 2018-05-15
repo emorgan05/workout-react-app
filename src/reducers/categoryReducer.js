8c332c30057f91924bc03552f252bd0e9d029181
emorgan05 <emorgan05@gmail.com>
2018-05-15 16:04:10 -0400

    Changing initial state

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -1,10 +1,10 @@
-export function categoryReducer(state, action) {
+export function categoryReducer(state = {}, action) {
   switch(action.type) {
     case 'LOADING_CATEGORIES':
-      return Object.assign({}, state, {loading: true});
+      return Object.assign({}, {loading: true});
 
     case 'FETCH_CATEGORIES':
-      return Object.assign({}, state, {loading: false, categories: [].concat(state.categories, action.payload) });
+      return Object.assign({}, {loading: false, categories: [].concat(action.payload) });
 
     default:
       return state;
